67a68c1ecea6f8e36ac74af48c41cc3d8e134140
root <rinad.aliyev@github.com>
2025-11-17 11:37:24 +0000

    -

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-#data
 def element_at(my_list, idx):
-   if idx< 0:
-	return None
-   elif idx > len(my_list):
-	return None
-   else:
-	print("{}".format(mylist[idx])
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
