1420239bbd538392a55ab9c0c4b0cc60c2388c35
root <rinad.aliyev@github.com>
2025-11-17 09:17:35 +0000

    -

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-from add_0 import add
-
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a,b,result))
-
+_import_("0-add")
