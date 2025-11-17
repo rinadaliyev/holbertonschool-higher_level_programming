582a79a005071ca2c029597a4e787b927f66cb56
root <rinad.aliyev@github.com>
2025-11-17 11:43:16 +0000

    -

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    my_list[idx] = element
