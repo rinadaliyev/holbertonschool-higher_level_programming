30f0a3f80e9fa39e115a7db090d64edd753bcf5d
rinadaliyev <rinad.aliyevv@gmail.com>
2025-11-26 01:13:04 -0500

    -

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that defines a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with an area method placeholder."""
+
+    def area(self):
+        """Raise an exception when area is not implemented."""
+        raise Exception("area() is not implemented")
